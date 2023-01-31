94937ae95257ea340570ba6c3ede6dfe49fef05e
thaomaidang <38590744+thaomaidang@users.noreply.github.com>
2023-02-01 05:45:53 +0700

    Update convert_hex_to_bin.py

--- FEHash/bi_NIST_512_beacon/convert_hex_to_bin.py
+++ FEHash/bi_NIST_512_beacon/convert_hex_to_bin.py
@@ -21,8 +21,7 @@ def read_hex_code(path):
 
     return codes
 
-path = r'C:\Users\ThaoDang\PycharmProjects\LSH-EC\hex_NIST_1_108000.txt'
-out = r'C:\Users\ThaoDang\PycharmProjects\LSH-EC\bi_NIST_512_beacon'
+path = r'hex_NIST_1_108000.txt'
 hex_codes = read_hex_code(path)
 print(len(hex_codes))
 bi = []
@@ -32,13 +31,9 @@ for i in range(len(hex_codes)):
     binary = hextobin(extract)
 
     bi.append(binary)
-    #text_file = open("bi_NIST_19808_73038.txt", "a")
-    #text_file.write(binary)
-    #print('\n', end='', file=text_file)
-    #text_file.close()
 
 
-text_file = open(out + "\\bi_NIST_1_10800.txt", "w")
+text_file = open("bi_NIST_1_10800.txt", "w")
 for i in range(len(bi)):
     print('%s' % bi[i], file=text_file)
 text_file.close()
